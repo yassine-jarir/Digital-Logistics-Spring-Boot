fb78b8183694787443a605dc49339af856aa2342
yassine <yassinejarir7@gmail.com>
2025-11-24 14:57:58 +0100

    fix bugs

--- src/test/java/com/logistic/digitale_logistic/DigitaleLogisticApplicationTests.java
+++ src/test/java/com/logistic/digitale_logistic/DigitaleLogisticApplicationTests.java
@@ -3,7 +3,7 @@ package com.logistic.digitale_logistic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-///,,.
+///,,..
 @SpringBootTest
 @ActiveProfiles("test")
 class DigitaleLogisticApplicationTests {
