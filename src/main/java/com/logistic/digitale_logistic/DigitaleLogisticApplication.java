32666b0a47d76058b80d6d28d21c2b9aaf6f2845
yassine <yassinejarir7@gmail.com>
2025-12-25 14:39:52 +0100

    test integration

--- src/main/java/com/logistic/digitale_logistic/DigitaleLogisticApplication.java
+++ src/main/java/com/logistic/digitale_logistic/DigitaleLogisticApplication.java
@@ -3,7 +3,8 @@ package com.logistic.digitale_logistic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
- @SpringBootApplication(scanBasePackages = "com.logistic.digitale_logistic")
+//@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.logistic.digitale_logistic")
 
 public class DigitaleLogisticApplication {
 
--- src/main/java/com/logistic/digitale_logistic/config/SecurityConfig.java
+++ src/main/java/com/logistic/digitale_logistic/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.logistic.digitale_logistic.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
