41060be7c7d1d716fe0e7ca8e84f6c02a36f4f69
yassine <yassinejarir7@gmail.com>
2025-12-29 09:15:49 +0100

    jenkins

---
+++ Dockerfile.jenkins
@@ -0,0 +1,9 @@
+FROM jenkins/jenkins:lts
+
+USER root
+
+RUN apt-get update && \
+    apt-get install -y docker.io && \
+    apt-get clean
+
+USER jenkins
--- src/main/java/com/logistic/digitale_logistic/mapper/ShipmentMapper.java
+++ src/main/java/com/logistic/digitale_logistic/mapper/ShipmentMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {ShipmentLineMapper.class})
-public interface git ShipmentMapper {
+public interface  ShipmentMapper {
 
     @Mapping(source = "salesOrder.id", target = "salesOrderId")
     @Mapping(source = "salesOrder.orderNumber", target = "salesOrderNumber")
